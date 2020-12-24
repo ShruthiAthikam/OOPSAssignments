bdefbb88d10a5d85d1065b9e5bf05f9e97696b9e
ShruthiAthikam <71416001+ShruthiAthikam@users.noreply.github.com>
2020-12-23 19:09:51 -0500

    Update Ass6 - WinformApp - Moving-Object-Form1.cs

--- Ass6 - WinformApp - Moving-Object-Form1.cs
+++ Ass6 - WinformApp - Moving-Object-Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,142 +8,95 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Ass5_vs
+namespace Ass6
 {
-    
     public partial class Form1 : Form
     {
-        Random r = new Random();
-        int btn_xCount = 0;
-        int winCount = 0;
-        int lostCount = 0;
-        bool firsttime = true;
-        
+        int count = 0;
+        Form2 f2;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btn_x1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-           
-            timer1.Enabled = false;
-            btn_xCount++;
-            MessageDisplay();
+                switch (count)
+                {
+                    case 0:
+                        {
+                            picBox1.Image = Properties.Resources.Tampa;
+                            count = 1;
+                            this.Text = "Tampa";
+                            break;
+                        }
+                    case 1:
+                        {
+                            picBox1.Image = Properties.Resources.Orlando;
+                            count = 2;
+                            this.Text = "Orlando";
+                            break;
+                        }
+                    case 2:
+                        {
+                            picBox1.Image = Properties.Resources.Miami;
+                            count = 3;
+                            this.Text = "Miami";
+                            break;
+                        }
+                    case 3:
+                        {
+                            picBox1.Image = Properties.Resources.Jacksonville;
+                            count = 0;
+                            this.Text = "Jacksonville";
+                            break;
+                        }
+                }
         }
 
-        private void btn_x2_Click(object sender, EventArgs e)
-        {
-            timer2.Enabled = false;
-            btn_xCount++;
-            MessageDisplay();
-        }
+        
 
-        private void btn_x3_Click(object sender, EventArgs e)
+        private void cb1_CheckedChanged(object sender, EventArgs e)
         {
-            timer3.Enabled = false;
-            btn_xCount++;
-            MessageDisplay();
-        }
-
-        private void btnSpin_Click(object sender, EventArgs e)
-        {
-            if (firsttime)
+            if (cb1.Checked)
             {
-                this.Text = "Win: 0  Lose: 0";
-                firsttime = false;
-            }
-            btn_xCount = 0;
-            timer1.Enabled = timer2.Enabled = timer3.Enabled = true;
-        }
-
-        private void btnFlip_Click(object sender, EventArgs e)
-        {
-            if (btnFlip.Text == "Flip twice/second")
-            {
-                timer1.Interval = timer2.Interval = timer3.Interval = 500;
-                btnFlip.Text = "Flip once/second";
+                timer1.Enabled = false;
+                
             }
             else
             {
-                timer1.Interval = timer2.Interval = timer3.Interval = 1000;
-                btnFlip.Text = "Flip twice/second";
-            }
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            ChangingPictures(pic1);
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            ChangingPictures(pic2);
-        }
-
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            ChangingPictures(pic3);
-        }
-
-        private void Form1_DoubleClick(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void MessageDisplay()
-        {
-            if (btn_xCount == 3)
-            {
-               if ((pic1.Tag == pic2.Tag) && (pic2.Tag == pic3.Tag))
-                {
-                    MessageBox.Show("You win!!!");
-                    winCount++;
-                }
-                else
-                {
-                    MessageBox.Show("Try again...");
-                    lostCount++;
-                }
-
-                this.Text = "Win: " + winCount + " Lose: " + lostCount;
+                timer1.Enabled = true;
                 
             }
         }
 
-
-
-        private void ChangingPictures(PictureBox c)
+        private void picBox1_Click(object sender, EventArgs e)
         {
-            int randNo = r.Next(4);
-            switch (randNo)
-            {
-                case 0:
-                    {
-                        c.Image = Properties.Resources.Banana;
-                        c.Tag = "Banana";
-                        break;
-                    }
-                case 1:
-                    {
-                        c.Image = Properties.Resources.Cherry;
-                        c.Tag = "Cherry";
-                        break;
-                    }
-                case 2:
-                    {
-                        c.Image = Properties.Resources.Orange;
-                        c.Tag = "Orange";
-                        break;
-                    }
-                case 3:
-                    {
-                        c.Image = Properties.Resources.Watermelon;
-                        c.Tag = "Watermelon";
-                        break;
-                    }
-            }
+            this.Hide();
+            f2 = new Form2();
+            f2.ShowDialog();
+        }
 
+        private void openNextFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            picBox1_Click(sender, e);
+        }
+
+        private void freezePictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            cb1.Checked = true;
+        }
+
+        private void unfreezePictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            cb1.Checked = false;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
