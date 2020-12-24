29abbd71c3978a2a4018001d870f8671185a4f22
ShruthiAthikam <71416001+ShruthiAthikam@users.noreply.github.com>
2020-12-23 19:09:12 -0500

    Update Ass6 - WinformApp - Moving-Object-Form1.Designer.cs

--- Ass6 - WinformApp - Moving-Object-Form1.Designer.cs
+++ Ass6 - WinformApp - Moving-Object-Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Ass5_vs
+namespace Ass6
 {
     partial class Form1
     {
@@ -29,150 +29,128 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pic1 = new System.Windows.Forms.PictureBox();
-            this.pic2 = new System.Windows.Forms.PictureBox();
-            this.pic3 = new System.Windows.Forms.PictureBox();
-            this.btn_x1 = new System.Windows.Forms.Button();
-            this.btnSpin = new System.Windows.Forms.Button();
-            this.btnFlip = new System.Windows.Forms.Button();
-            this.btn_x3 = new System.Windows.Forms.Button();
-            this.btn_x2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.timer3 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            this.cb1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openNextFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.freezePictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unfreezePictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pic1
-            // 
-            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pic1.Image = global::Ass5_vs.Properties.Resources.Watermelon;
-            this.pic1.Location = new System.Drawing.Point(87, 47);
-            this.pic1.Name = "pic1";
-            this.pic1.Size = new System.Drawing.Size(139, 137);
-            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pic1.TabIndex = 0;
-            this.pic1.TabStop = false;
-            // 
-            // pic2
-            // 
-            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pic2.Image = global::Ass5_vs.Properties.Resources.Watermelon;
-            this.pic2.Location = new System.Drawing.Point(271, 47);
-            this.pic2.Name = "pic2";
-            this.pic2.Size = new System.Drawing.Size(140, 137);
-            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pic2.TabIndex = 1;
-            this.pic2.TabStop = false;
-            // 
-            // pic3
-            // 
-            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pic3.Image = global::Ass5_vs.Properties.Resources.Watermelon;
-            this.pic3.Location = new System.Drawing.Point(451, 47);
-            this.pic3.Name = "pic3";
-            this.pic3.Size = new System.Drawing.Size(139, 137);
-            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pic3.TabIndex = 2;
-            this.pic3.TabStop = false;
-            // 
-            // btn_x1
-            // 
-            this.btn_x1.AutoSize = true;
-            this.btn_x1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_x1.Location = new System.Drawing.Point(117, 216);
-            this.btn_x1.Name = "btn_x1";
-            this.btn_x1.Size = new System.Drawing.Size(75, 31);
-            this.btn_x1.TabIndex = 3;
-            this.btn_x1.Text = "x";
-            this.btn_x1.UseVisualStyleBackColor = true;
-            this.btn_x1.Click += new System.EventHandler(this.btn_x1_Click);
-            // 
-            // btnSpin
-            // 
-            this.btnSpin.AutoSize = true;
-            this.btnSpin.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSpin.Location = new System.Drawing.Point(647, 106);
-            this.btnSpin.Name = "btnSpin";
-            this.btnSpin.Size = new System.Drawing.Size(75, 39);
-            this.btnSpin.TabIndex = 6;
-            this.btnSpin.Tag = "";
-            this.btnSpin.Text = "Spin";
-            this.btnSpin.UseVisualStyleBackColor = true;
-            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
-            // 
-            // btnFlip
-            // 
-            this.btnFlip.AutoSize = true;
-            this.btnFlip.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFlip.Location = new System.Drawing.Point(256, 308);
-            this.btnFlip.Name = "btnFlip";
-            this.btnFlip.Size = new System.Drawing.Size(205, 39);
-            this.btnFlip.TabIndex = 7;
-            this.btnFlip.Text = "Flip twice/second";
-            this.btnFlip.UseVisualStyleBackColor = true;
-            this.btnFlip.Click += new System.EventHandler(this.btnFlip_Click);
-            // 
-            // btn_x3
-            // 
-            this.btn_x3.AutoSize = true;
-            this.btn_x3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_x3.Location = new System.Drawing.Point(489, 216);
-            this.btn_x3.Name = "btn_x3";
-            this.btn_x3.Size = new System.Drawing.Size(75, 31);
-            this.btn_x3.TabIndex = 8;
-            this.btn_x3.Text = "x";
-            this.btn_x3.UseVisualStyleBackColor = true;
-            this.btn_x3.Click += new System.EventHandler(this.btn_x3_Click);
-            // 
-            // btn_x2
-            // 
-            this.btn_x2.AutoSize = true;
-            this.btn_x2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_x2.Location = new System.Drawing.Point(306, 216);
-            this.btn_x2.Name = "btn_x2";
-            this.btn_x2.Size = new System.Drawing.Size(75, 31);
-            this.btn_x2.TabIndex = 9;
-            this.btn_x2.Text = "x";
-            this.btn_x2.UseVisualStyleBackColor = true;
-            this.btn_x2.Click += new System.EventHandler(this.btn_x2_Click);
             // 
             // timer1
             // 
-            this.timer1.Interval = 1000;
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // timer2
+            // cb1
             // 
-            this.timer2.Interval = 1000;
-            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            this.cb1.AutoSize = true;
+            this.cb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb1.Location = new System.Drawing.Point(87, 35);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(160, 29);
+            this.cb1.TabIndex = 1;
+            this.cb1.Text = "Freeze Picture";
+            this.cb1.UseVisualStyleBackColor = true;
+            this.cb1.CheckedChanged += new System.EventHandler(this.cb1_CheckedChanged);
             // 
-            // timer3
+            // menuStrip1
             // 
-            this.timer3.Interval = 1000;
-            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1095, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openNextFormToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.freezePictureToolStripMenuItem,
+            this.unfreezePictureToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.quitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // openNextFormToolStripMenuItem
+            // 
+            this.openNextFormToolStripMenuItem.Name = "openNextFormToolStripMenuItem";
+            this.openNextFormToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.openNextFormToolStripMenuItem.Text = "Open next form";
+            this.openNextFormToolStripMenuItem.Click += new System.EventHandler(this.openNextFormToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(213, 6);
+            // 
+            // freezePictureToolStripMenuItem
+            // 
+            this.freezePictureToolStripMenuItem.Name = "freezePictureToolStripMenuItem";
+            this.freezePictureToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.freezePictureToolStripMenuItem.Text = "Freeze Picture";
+            this.freezePictureToolStripMenuItem.Click += new System.EventHandler(this.freezePictureToolStripMenuItem_Click);
+            // 
+            // unfreezePictureToolStripMenuItem
+            // 
+            this.unfreezePictureToolStripMenuItem.Name = "unfreezePictureToolStripMenuItem";
+            this.unfreezePictureToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.unfreezePictureToolStripMenuItem.Text = "Unfreeze Picture";
+            this.unfreezePictureToolStripMenuItem.Click += new System.EventHandler(this.unfreezePictureToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(213, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // picBox1
+            // 
+            this.picBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBox1.Location = new System.Drawing.Point(0, 28);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(1095, 574);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            this.picBox1.Click += new System.EventHandler(this.picBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_x2);
-            this.Controls.Add(this.btn_x3);
-            this.Controls.Add(this.btnFlip);
-            this.Controls.Add(this.btnSpin);
-            this.Controls.Add(this.btn_x1);
-            this.Controls.Add(this.pic3);
-            this.Controls.Add(this.pic2);
-            this.Controls.Add(this.pic1);
+            this.ClientSize = new System.Drawing.Size(1095, 602);
+            this.ControlBox = false;
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
-            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,17 +158,17 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pic1;
-        private System.Windows.Forms.PictureBox pic2;
-        private System.Windows.Forms.PictureBox pic3;
-        private System.Windows.Forms.Button btn_x1;
-        private System.Windows.Forms.Button btnSpin;
-        private System.Windows.Forms.Button btnFlip;
-        private System.Windows.Forms.Button btn_x3;
-        private System.Windows.Forms.Button btn_x2;
+        private System.Windows.Forms.PictureBox picBox1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.CheckBox cb1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openNextFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem freezePictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unfreezePictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
     }
 }
 
